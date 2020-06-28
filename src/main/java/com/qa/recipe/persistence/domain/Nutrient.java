e0f19c7083f30b5606b580ede3ca08283fb9bbd2
Naomi Gabriel <naomi_gab@hotmail.com>
2020-06-28 15:00:33 +0100

    QHWA-7 #done added Nutrient entity

--- src/main/java/com/qa/recipe/persistence/domain/Nutrient.java
+++ src/main/java/com/qa/recipe/persistence/domain/Nutrient.java
@@ -4,70 +4,40 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Entity
-public class Ingredient {
-
-
+public class Nutrient {
+	
 	@Id
 	@GeneratedValue
 	private Long id;
 	
-	@Column(name = "ingredient_name", unique = true)
+	@Column(name = "nutrient_name", unique = true)
 	@NotNull
 	@Size(min = 0, max = 80)
 	private String name;
-	
-	
-	@ManyToOne(targetEntity = Ingredient.class)
-	private Nutrient nutrient;
 
+	public Nutrient(String name) {
 
-	public Ingredient() {
-		
-	}
-
-
-	public Ingredient(Long id, @NotNull @Size(min = 0, max = 80) String name) {
-		super();
-		this.id = id;
 		this.name = name;
-		//this.nutrient = nutrient;
 	}
-	
-	
+	public Nutrient() {
+
+	}
 	public Long getId() {
 		return id;
 	}
-
-
 	public void setId(Long id) {
 		this.id = id;
 	}
-
-
 	public String getName() {
 		return name;
 	}
-
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
-
-//	public Nutrient getNutrient() {
-//		return nutrient;
-//	}
-//
-//
-//	public void setNutrient(Nutrient nutrient) {
-//		this.nutrient = nutrient;
-//	}
-
+	
 
 }
