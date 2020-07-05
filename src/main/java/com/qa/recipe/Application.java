887cedf62bcfbc701206d53e8875f5e60ef613e3
Naomi Gabriel <naomi_gab@hotmail.com>
2020-07-05 23:34:09 +0100

    added create and read functionality on site

--- src/main/java/com/qa/recipe/Application.java
+++ src/main/java/com/qa/recipe/Application.java
@@ -5,7 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication(exclude = { MongoAutoConfiguration.class, MongoDataAutoConfiguration.class })
+@EnableSwagger2
 public class Application {
 
 	public static void main(String[] args) {
